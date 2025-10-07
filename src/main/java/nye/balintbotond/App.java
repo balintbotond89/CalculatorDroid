6e92359b1f328f01e4a74026c30f287eb4c047fc
balintbotond89 <balint.botond@student.nye.hu>
2025-10-07 19:15:33 +0200

    -> Maven installed and configured -> Checkstlyle configured -> Code cleansed

--- src/main/java/nye/balintbotond/App.java
+++ src/main/java/nye/balintbotond/App.java
@@ -1,13 +1,7 @@
 package nye.balintbotond;
-
-/**
- * Hello world!
- *
- */
+@SuppressWarnings({"PMD.SystemPrintln"})
 public class App 
-{
-    public static void main( String[] args )
+{ public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-    }
-}
+    }}
