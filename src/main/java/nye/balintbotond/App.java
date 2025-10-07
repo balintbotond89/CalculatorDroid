b027bab6cfa4324a1c8723db7d29bedf4151d206
balintbotond89 <balint.botond@student.nye.hu>
2025-10-07 19:40:53 +0200

    -> Changed firstNumber to 199 -> Changed secondNumber to 301 -> Added function substraction -> Added function multiplication -> Added function distribution -> Added function Residual distribution

--- src/main/java/nye/balintbotond/App.java
+++ src/main/java/nye/balintbotond/App.java
@@ -4,8 +4,17 @@ package nye.balintbotond;
 @SuppressWarnings({"PMD.SystemPrintln"})
 public class App {
     public static void main(String[] args) {
-        int firstNumber = 12;
-        int secondNumber = 55;
+        int firstNumber = 119;
+        int secondNumber = 301;
+        //Addition (Összeadás):
         System.out.println(firstNumber + secondNumber);
+        //Substraction (Kivonás):
+        System.out.println(firstNumber - secondNumber);
+        //Multiplication (Szorzás):
+        System.out.println(firstNumber * secondNumber);
+        //Distribution (Egész számos osztás):
+        System.out.println(secondNumber / firstNumber);
+        //Residual distribution (Maradékos osztás):
+        System.out.println(secondNumber % firstNumber);
     }
 }
