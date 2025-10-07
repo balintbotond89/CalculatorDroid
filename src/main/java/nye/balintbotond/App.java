535b014c96ec007478d1b097e81a7f0a237c3b5e
balintbotond89 <balint.botond@student.nye.hu>
2025-10-07 19:30:31 +0200

    -> Added two number integers 12 and 24 -> Goal: print out 36

--- src/main/java/nye/balintbotond/App.java
+++ src/main/java/nye/balintbotond/App.java
@@ -1,8 +1,11 @@
 package nye.balintbotond;
+
+//Surpress false warning of PMD for println
 @SuppressWarnings({"PMD.SystemPrintln"})
-public class App 
-{ public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }}
-//Test commit
+public class App {
+    public static void main(String[] args) {
+        int firstNumber = 12;
+        int secondNumber = 24;
+        System.out.println(firstNumber + secondNumber);
+    }
+}
