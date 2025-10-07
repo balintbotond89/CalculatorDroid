a76308f367549fdfe51afe8fbae6b8d6f5841f32
balintbotond89 <balint.botond@student.nye.hu>
2025-10-07 20:56:08 +0200

    -> Added import java.util.Scanner to get user input -> Created object for scanner input -> Input read to droid.setFirstNumber and setSecondNumber -> Close scanner input session

--- src/main/java/nye/balintbotond/App.java
+++ src/main/java/nye/balintbotond/App.java
@@ -1,17 +1,28 @@
 package nye.balintbotond;
 
+import java.util.Scanner; //required to read user inputs
+
 //Suppress false warning of PMD for println
 @SuppressWarnings({"PMD.SystemPrintln"})
 public class App {
     public static void main(String[] args) {
-        //We create a datatype just like we would create an int
+        //Create the object for the scanner input
+        Scanner input = new Scanner(System.in);
+
+        //We create a datatype just like we would create an int:
         CalculatorDroid droid = new CalculatorDroid();
-        //From now on we can use this datatype
-        droid.setFirstNumber(34);
-        droid.setSecondNumber(56);
+
+        //From now on we can use this datatype:
+        droid.setFirstNumber(input.nextInt());
+        droid.setSecondNumber(input.nextInt());
+
+        //Closing the scanner input session:
+        input.close();
+
         System.out.println(droid.getFirstNumber());
         System.out.println(droid.getSecondNumber());
 
+        //We adress the methods
         droid.addNumbers();
         droid.subtractNumbers();
         droid.divideNumbers();
