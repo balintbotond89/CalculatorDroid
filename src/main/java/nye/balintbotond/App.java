464e72af431caeb6e0e675f57d9200ee4dd48563
balintbotond89 <balint.botond@student.nye.hu>
2025-10-07 20:18:14 +0200

    -> Scratched current project -> Cleansed Main -> Added class: CalculatorDroid -> Added private int firstNumber -> Added private int secondNumber -> Added Setter method for firstNumber -> Added Getter method for firstNumber -> Added Setter method for secondNumber -> Added Getter method for secondNumber

--- src/main/java/nye/balintbotond/App.java
+++ src/main/java/nye/balintbotond/App.java
@@ -1,20 +1,9 @@
 package nye.balintbotond;
 
-//Surpress false warning of PMD for println
+//Suppress false warning of PMD for println
 @SuppressWarnings({"PMD.SystemPrintln"})
 public class App {
     public static void main(String[] args) {
-        int firstNumber = 119;
-        int secondNumber = 301;
-        //Addition (Összeadás):
-        System.out.println(firstNumber + secondNumber);
-        //Substraction (Kivonás):
-        System.out.println(firstNumber - secondNumber);
-        //Multiplication (Szorzás):
-        System.out.println(firstNumber * secondNumber);
-        //Distribution (Egész számos osztás):
-        System.out.println(secondNumber / firstNumber);
-        //Residual distribution (Maradékos osztás):
-        System.out.println(secondNumber % firstNumber);
+
     }
 }
---
+++ src/main/java/nye/balintbotond/CalculatorDroid.java
@@ -0,0 +1,28 @@
+package nye.balintbotond;
+
+public class CalculatorDroid {
+
+    //Information we should keep hidden outside this the current class. Goal: Encapsulation.
+    private int firstNumber;
+    private int secondNumber;
+
+    //Setter method to request information (void methódus, mert nincs visszatérési értéke)
+    public void setFirstNumber(int number) {
+        firstNumber=number;
+    }
+
+    //Getter method to change information (methódus, adat megváltoztatására)
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    //Setter method for secondNumber
+    public void setSecondNumber(int number) {
+        secondNumber=number;
+    }
+
+    //Getter method for secondNumber
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+}
